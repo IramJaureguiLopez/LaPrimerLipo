47483e7bc8181d8d3fa871dc24bdaa1c787c1140
Jauregui Lopez Iram <iram.jauregui.lopez@uabc.edu.mx>
2026-04-14 10:38:10 -0700

    Add main menu for LaPrimerLipo application
    Implement main menu for price consultation and appointment scheduling.

---
+++ LaPrimerLipo.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{  
+int precio,opc;
+float preciodescuento;
+puts("MENU");
+puts("1. Consultar precios");
+puts("2. Agendar una cita");
+puts("3. Salir");
+puts("Que opcion quieres? ");
+scanf("%d",&opc);  
+do 
+{
+  case 1:
+  puts("Precio        Zonas a cubrir                   Cuidados post operatorio");
+  puts("------------------------------------------------------------------------------");
+  puts("45,000        Abdomen o papada                        N/A ");
+  puts("100,000    Abdomen,papada,brazos,esopalda       Fajas, medicamentos ");
+  puts("140,000    Abdomen,papada,brazos,esopalda       Fajas, masajes linfaticos,
+                         muslos y axilas                 medicamentos");       
